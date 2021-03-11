78d41280e3feb855cc2f4e83d3e1c898d8c2699f
Mustafa Faruk Esen <mustafarukesen@gmail.com>
2021-03-11 22:48:01 +0300

    11 Mart

---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CarDetail } from 'src/app/models/carDetail';
+import { CarService } from 'src/app/services/car.service';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  cars:CarDetail[] = [];
+  dataLoaded = false;
+
+  constructor(private carService:CarService) { }
+
+  ngOnInit(): void {
+    this.getCars();
+  }
+
+  getCars(){
+    this.carService.getCars().subscribe((response) => {
+      this.cars = response.data,
+      this.dataLoaded = true;
+    })
+  }
+
+}
---
+++ src/app/models/rentalDetail.ts
@@ -0,0 +1,8 @@
+export interface RentalDetail{
+    rentalId:number,
+    brandName:string,
+    customerName:string,
+    rentDate:string,
+    returnDate:string
+    
+}
